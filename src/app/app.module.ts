a808ce0013bf5bae6ea896631f44e4b390813ffc
Charlotte Jessegus <charlotte.jessegus@dkd.de>
2020-03-18 09:07:29 +0100

    add dropdown menu to nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-/*import { HttpClientModule } from '@angular/common/http';*/
+import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +24,7 @@ import { HeaderComponent } from './components/header/header.component';
     BrowserModule,
     FormsModule,
     NgbModule,
-    /*HttpClientModule,*/
+    HttpClientModule,
     AppRoutingModule,
   ],
   providers: [],
